923a0c4efb67bb82157adb105fba92c8822b89f1
amitmodi <amitmodi038@gmail.com>
2024-11-03 18:36:10 +0530

    files added

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("running");
+
+const express= require("express");
+
+const app=express();
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server");
+})
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the server seervere");
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello from the server");
+})
+
+app.listen(3000,()=>{
+    console.log("server is listeing at port 3000");
+});
