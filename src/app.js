70f60d37cf9d751b1d07f69e22bb1613450dd346
amitmodi <amitmodi038@gmail.com>
2024-11-07 18:57:37 +0530

    chaned in app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express= require("express");
 const app=express();
 
 app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
+    res.send("Hello from the server-test");
 })
 
 app.use("/",(req,res)=>{
