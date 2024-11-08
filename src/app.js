934c4dd0bdefc09011135ce675051ab79d51d481
amitmodi <amitmodi038@gmail.com>
2024-11-08 09:11:20 +0530

    played with regex, adding some operator,params,query

--- src/app.js
+++ src/app.js
@@ -5,8 +5,33 @@ const express= require("express");
 const app=express();
 
 //this will handle get call to /user
-app.get("/user",(req,res)=>{
+// /*app.get("/user",(req,res)=>{
+//     res.send("hello frm the user");
+// })*/
+
+
+app.get("/user/:userid",(req,res)=>{
+    console.log(req.params);    
     res.send("hello frm the user");
+     })
+
+app.use("/profil?es",(req,res)=>{
+    res.send("profile founded");
+})
+
+app.use("/ab+c",(req,res)=>{
+    res.send("abc done ");
+})
+
+app.use("/ab*cd",(req,res)=>{
+    res.send("abcd  multiplied done ");
+})
+
+
+// "/asfaffly?name=amit&id=43"
+app.use(/.*fly$/,(req,res)=>{
+    console.log(req.query);
+    res.send("anything ends with fly works here   ");
 })
 
 app.post("/user",(req,res)=>{
